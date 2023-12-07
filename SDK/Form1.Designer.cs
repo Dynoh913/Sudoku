6e8ab0cd8ee646099bc60e714632603815c25235
Hoàng Mạnh Tuấn <93393820+Dynoh913@users.noreply.github.com>
2023-12-07 15:28:15 +0700

    Add files via upload

---
+++ SDK/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SDK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Solve = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(649, 594);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sudoku";
+            // 
+            // Solve
+            // 
+            this.Solve.Location = new System.Drawing.Point(155, 617);
+            this.Solve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(117, 48);
+            this.Solve.TabIndex = 1;
+            this.Solve.Text = "Solve";
+            this.Solve.UseVisualStyleBackColor = true;
+            this.Solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(280, 617);
+            this.buttonImport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(117, 48);
+            this.buttonImport.TabIndex = 2;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(16, 617);
+            this.buttonNew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(131, 48);
+            this.buttonNew.TabIndex = 4;
+            this.buttonNew.Text = "New";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(405, 617);
+            this.buttonExport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(121, 48);
+            this.buttonExport.TabIndex = 5;
+            this.buttonExport.Text = "Export";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(484, 181);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 3;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(534, 617);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(131, 48);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Exit";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 684);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.Solve);
+            this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Sudoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+    }
+
+      
+    }
+
+
---
+++ SDK/Form1.cs
@@ -0,0 +1,510 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SDK
+{
+    public partial class Form1 : Form
+    {
+        public int n = 9;
+        public Button[][] b;
+        public int sz = 45;
+        struct current
+        {
+            public int x;
+            public int y;
+        }
+        current acurrent = new current();
+        //Boolean start = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region[FormLoad]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createSudoku();
+            acurrent.x = -1;
+            acurrent.y = -1;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            buttonInput_KeyDown(sender, e);
+
+        }
+        #endregion
+        #region[createSDK]
+        public void createSudoku()
+        {
+            b = new Button[n][];
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = new Button[n];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    b[i][j] = new Button();
+                    b[i][j].Size = new Size(sz, sz);
+                    if (i < 3 && j < 3)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else if (i >= 3 && i < 6 && j >= 3 && j < 6)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else if (i >= 6 && i < 9 && j >= 6 && j < 9)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else if (i >= 6 && i < 9 && j < 3)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else if (i >= 6 && i < 9 && j >= 6)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else if (i < 3 && j >= 6 && j < 9)
+                    {
+                        b[i][j].BackColor = Color.FromName("Cornsilk");
+                    }
+                    else
+                    {
+                        b[i][j].BackColor = Color.White;
+                    }
+                    b[i][j].Text = " ";
+                    b[i][j].ForeColor = Color.FromName("red");
+                    b[i][j].Location = new Point(i * sz + sz, j * sz + sz);
+                    b[i][j].Click += new EventHandler(button_Click);
+                    groupBox1.Controls.Add(b[i][j]);
+                }
+            }
+        }
+        #endregion
+        #region[buttonClick]
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            int x = acurrent.x;
+            int y = acurrent.y;
+            if (x >= 0 && y >= 0 && x <= 8 && y <= 8)
+            {
+                Button bt = (Button)sender;
+                if (bt.Text != "0")
+                {
+                    b[x][y].Text = bt.Text;
+                }
+                else
+                {
+                    b[x][y].Text = " ";
+                }
+                acurrent.x = -1;
+                acurrent.y = -1;
+            }
+        }
+        private void buttonInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            int x = acurrent.x;
+            int y = acurrent.y;
+            if (x >= 0 && y >= 0 && x <= 8 && y <= 8)
+            {
+                string text = e.KeyData.ToString();
+                int result = checkKeyData(text);
+                if (result >= 0 && result <= 9)
+                {
+                    if (result != 0)
+                    {
+                        b[x][y].Text = result.ToString();
+                    }
+                    else
+                    {
+                        b[x][y].Text = " ";
+                    }
+                    acurrent.x = -1;
+                    acurrent.y = -1;
+                }
+            }
+        }
+        
+        private void button_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            Button bt = (Button)sender;
+            //bt.Text = (int.Parse(bt.Text) + 1).ToString();
+            int x = int.Parse(bt.Location.X.ToString());
+            x = x / 45 - 1;
+            int y = int.Parse(bt.Location.Y.ToString());
+            y = y / 45 - 1;
+            acurrent.x = x;
+            acurrent.y = y;
+
+        }
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            acurrent.x = -1;
+            acurrent.y = -1;
+            Import();
+        }
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            acurrent.x = -1;
+            acurrent.y = -1;
+            Clear();
+        }
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            acurrent.x = -1;
+            acurrent.y = -1;
+            Export();
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+        #region[Solve]
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            acurrent.x = -1;
+            acurrent.y = -1;
+            if (checkInput() == false)
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại đề đã nhập!");
+
+            }
+            else
+            {
+                //start = true;
+                int[,] arr = new int[9, 9];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (b[i][j].Text != " ")
+                        {
+                            b[i][j].ForeColor = Color.FromName("Black");
+
+                        }
+                        if (b[i][j].Text != " ")
+                        {
+                            arr[i, j] = int.Parse(b[i][j].Text);
+                        }
+                        else
+                        {
+                            arr[i, j] = 0;
+                        }
+                        b[i][j].Click -= new EventHandler(button_Click);
+                    }
+                }
+                int[,] arr2 = arr;
+                if (solve(arr, 0, 0))
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (arr[i, j].ToString() != "0")
+                            {
+                                b[i][j].Text = arr[i, j].ToString();
+                            }
+                            else
+                            {
+                                b[i][j].Text = " ";
+                            }
+                        }
+                    }
+                }
+                Solve.Enabled = false;
+            }
+        }
+        public static Boolean solve(int[,] arr, int row, int col)
+        {
+            if (row < 9 && col < 9)
+            {
+                if (arr[row, col] != 0)
+                {
+                    if ((col + 1) < 9) return solve(arr, row, col + 1);
+                    else if ((row + 1) < 9) return solve(arr, row + 1, 0);
+                    else return true;
+                }
+                else
+                {
+                    for (int i = 0; i < 9; ++i)
+                    {
+                        if (IsAvailable(arr, row, col, i + 1))
+                        {
+                            arr[row, col] = i + 1;
+
+                            if ((col + 1) < 9)
+                            {
+                                if (solve(arr, row, col + 1)) return true;
+                                else arr[row, col] = 0;
+                            }
+                            else if ((row + 1) < 9)
+                            {
+                                if (solve(arr, row + 1, 0)) return true;
+                                else arr[row, col] = 0;
+                            }
+                            else return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            else return true;
+        }
+        #endregion
+        #region[CheckAvailabe]
+        private static bool IsAvailable(int[,] puzzle, int row, int col, int num)
+        {
+            int rowStart = (row / 3) * 3;
+            int colStart = (col / 3) * 3;
+
+            for (int i = 0; i < 9; ++i)
+            {
+                if (puzzle[row, i] == num) return false;
+                if (puzzle[i, col] == num) return false;
+                if (puzzle[rowStart + (i % 3), colStart + (i / 3)] == num) return false;
+            }
+
+            return true;
+        }
+        #endregion
+        #region[Checkinput]
+        public Boolean checkInput() //check đề nhập vào có trùng cột, hàng, trong ô hay không.
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (b[i][j].Text != " ")
+                    {
+                        for (int k = i + 1; k < 9; ++k)  // kiem tra theo hang
+                        {
+                            if (b[k][j].Text != " ")
+                            {
+                                if (int.Parse(b[i][j].Text) == int.Parse(b[k][j].Text))
+                                    return false;
+                            }
+                        }
+                        for (int k = 0; k < i; ++k)  // kiem tra theo hang
+                        {
+                            if (b[k][j].Text != " ")
+                            {
+                                if (int.Parse(b[i][j].Text) == int.Parse(b[k][j].Text))
+                                    return false;
+                            }
+                        }
+                        for (int k = j + 1; k < 9; ++k)  // kiem tra theo hang
+                        {
+                            if (b[i][k].Text != " ")
+                            {
+                                if (int.Parse(b[i][j].Text) == int.Parse(b[i][k].Text))
+                                    return false;
+                            }
+                        }
+                        for (int k = 0; k < j; ++k)  // kiem tra theo hang
+                        {
+                            if (b[i][k].Text != " ")
+                            {
+                                if (int.Parse(b[i][j].Text) == int.Parse(b[i][k].Text))
+                                    return false;
+                            }
+                        }
+                        int boxRowOffset = (i / 3) * 3;
+                        int boxColOffset = (j / 3) * 3;
+
+                        for (int k = 0; k < 3; ++k) //kiem tra trong 9 ô nhỏ
+                            for (int m = 0; m < 3; ++m)
+                                if ((boxRowOffset + k) != i && boxColOffset + m != j)
+                                {
+                                    if (b[boxRowOffset + k][boxColOffset + m].Text != " ")
+                                    {
+                                        if (int.Parse(b[i][j].Text) == int.Parse(b[boxRowOffset + k][boxColOffset + m].Text))
+                                            return false;
+                                    }
+                                }
+                    }
+                }
+            }
+            return true;
+        }
+        #endregion
+        #region[Import]     
+        private void Import()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "txt file|*.txt";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Clear();
+                string filename = op.FileName;
+                label1.Text = "";
+                string[] filelines = File.ReadAllLines(filename);
+                if (filelines.Length == 9)
+                {
+                    for (int j = 0; j < filelines.Length; j++)
+                    {
+                        string[] splitLines = filelines[j].Split(' ');
+                        if (splitLines.Length == 9)
+                        {
+                            for (int i = 0; i < splitLines.Length; i++)
+                            {
+                                if (int.Parse(splitLines[i]) >= 0 && int.Parse(splitLines[i]) <= 9)
+                                {
+                                    if (int.Parse(splitLines[i]) == 0)
+                                    {
+                                        b[i][j].Text = " ";
+                                        b[i][j].ForeColor = Color.FromName("red");
+                                    }
+                                    else
+                                    {
+                                        b[i][j].Text = splitLines[i];
+                                        b[i][j].ForeColor = Color.FromName("black");
+                                        b[i][j].Click -= new EventHandler(button_Click);
+                                    }
+                                }
+                                else
+                                {
+                                    label1.Text = "Error Reading File";
+                                    Clear();
+                                    return;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            label1.Text = "Error Reading File";
+                            Clear();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    label1.Text = "Error Reading File";
+                    Clear();
+                    return;
+                }
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+        #region[Reset]
+        private void Clear()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    b[i][j].Text = " ";
+                    b[i][j].ForeColor = Color.FromName("red");
+                    b[i][j].Click -= new EventHandler(button_Click);
+                    b[i][j].Click += new EventHandler(button_Click);
+                }
+            }
+            Solve.Enabled = true;
+        }
+        #endregion
+        #region[Export]
+        private void Export()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog.FileName;
+                string[] contents = new string[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    contents[j] = "";
+                    for (int i = 0; i < 9; i++)
+                    {
+                        string anum = "";
+                        if (b[i][j].Text == " ")
+                        {
+                            anum = "0";
+                        }
+                        else
+                        {
+                            anum = b[i][j].Text;
+                        }
+                        if (i < 8)
+                        {
+                            contents[j] += anum + " ";
+                        }
+                        else
+                        {
+                            contents[j] += anum;
+                        }
+                    }
+                }
+                File.WriteAllLines(filename, contents);
+            }
+        }
+        #endregion
+        
+
+        
+        private int checkKeyData(string data)
+        {
+            switch (data)
+            {
+                case "D1":
+                case "NumPad1": return 1;
+                case "D2":
+                case "NumPad2": return 2;
+                case "D3":
+                case "NumPad3": return 3;
+                case "D4":
+                case "NumPad4": return 4;
+                case "D5":
+                case "NumPad5": return 5;
+                case "D6":
+                case "NumPad6": return 6;
+                case "D7":
+                case "NumPad7": return 7;
+                case "D8":
+                case "NumPad8": return 8;
+                case "D9":
+                case "NumPad9": return 9;
+                case "D0":
+                case "NumPad0":
+                case "Back":
+                case "Delete": return 0;
+                default: return -1;
+            }
+        }
+        
+
+
+    }
+}
+
+
